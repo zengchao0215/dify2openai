2b38803f021163e0523e75044afacbe51055c934
凌渊之尘 <72078405+lingyuanzhicheng@users.noreply.github.com>
2024-06-21 15:10:38 +0800

    add /v1/models

--- app.js
+++ app.js
@@ -68,6 +68,21 @@ app.get('/', (req, res) => {
   `);
 });
 
+app.get('/v1/models', (req, res) => {
+  const models = {
+    "object": "list",
+    "data": [
+      {
+        "id": process.env.MODELS_NAME || "dify",
+        "object": "model",
+        "owned_by": "dify",
+        "permission": null,
+      }
+    ]
+  };
+  res.json(models);
+});
+
 app.post("/v1/chat/completions", async (req, res) => {
   const authHeader =
     req.headers["authorization"] || req.headers["Authorization"];
